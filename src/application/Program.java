facd44aaf6be1aafc127122e3f5f256cb6dba53b
Paolo Domenico <paolodomenico777@gmail.com>
2022-04-08 02:43:33 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,11 @@ public class Program {
         Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newseller);
         System.out.println("Inserted ! New id = " + newseller.getId());
+        System.out.println();
+        System.out.println("=== TEST 4: seller update===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
